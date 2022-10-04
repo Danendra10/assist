ef95bd1d585ca74b40cfb1261f5f021a07e4cb09
Danendra Clevero Ananda <danendraclever24@gmail.com>
2022-10-05 00:36:32 +0700

    first update for basic functionallity

---
+++ include/assist/assist.h
@@ -0,0 +1,38 @@
+#ifndef ASSIST_H
+#define ASSIST_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "assist/BS_utils.h"
+
+    // Data to be proceed to make robot's decision
+    typedef struct assist_data_tag
+    {
+        int16_t robot_x[6];
+        int16_t robot_y[6];
+        int16_t robot_th[6];
+        uint8_t robot_num;
+        int16_t ball_x;
+        int16_t ball_y;
+        char obs[15];
+        uint8_t total_obs;
+        uint8_t game_status;
+
+    } assist_data_t;
+
+    void AssistPrepKickoffHome(assist_data_t *data, uint8_t *ret);
+    void AssistKickoffHome(assist_data_t *data, uint8_t *ret);
+
+    void AssistRun(assist_data_t *data, uint8_t *ret);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
